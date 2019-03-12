14bf10a6f6614505296c2ba64df0a3b484a8f5a1
liu <liuyiguang8018@gmail.com>
2019-03-12 15:42:38 +0800

    ...

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
     cv::Mat unitsImg = cv::imread("../TangramSolver/unitPatterns/units4.jpg",0);
     
     //读入目标图像
-    cv::Mat dstsImg = cv::imread("../TangramSolver/dstPatterns/d4/03.jpg",0);
+    cv::Mat dstsImg = cv::imread("../TangramSolver/dstPatterns/d4/01.jpg",0);
     
     TangramSolver solver;
     solver.setFlipEnable(false);
